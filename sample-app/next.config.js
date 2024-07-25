cd89e3221de34d4c45bacba00922a74a48165ea8
Thorben Westerhuys <thorben.westerhuys@gmail.com>
2024-07-25 09:23:20 +0000

    fix: gitlab pages next base path

--- sample-app/next.config.js
+++ sample-app/next.config.js
@@ -7,6 +7,7 @@ await import('./src/env.js');
 /** @type {import("next").NextConfig} */
 const config = {
   output: 'export',
+  basePath: process.env.NEXT_PUBLIC_PUBLIC_URL ? new URL(process.env.NEXT_PUBLIC_PUBLIC_URL).pathname.replace(/\/$/, '') : '',
   experimental: {
     missingSuspenseWithCSRBailout: false
   }
